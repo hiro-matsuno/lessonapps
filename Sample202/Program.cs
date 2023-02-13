a05fbc2d5fee7c34576f7ca05f787af4c2cd863a
Hironori Matsuno <hiro.matsuno@gmail.com>
2023-02-13 06:47:27 +0000

    2023-02-13 15:47時点

--- Sample202/Program.cs
+++ Sample202/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample202{
+    class program{
+        static void Main(string[] args){
+            int a;
+            int b = 3;
+            int add, sub;
+            double avg;
+            a = 6;
+            add = a + b;
+            sub = a - b;
+            avg = (a + b) / 2.0;
+            //Console.WriteLine("変数num1は{0}、変数num2は{1}", a, b);
+            Console.WriteLine("{0} + {1} = {2}", a, b, add);
+            Console.WriteLine("{0} - {1} = {2}", a, b, sub);
+            Console.WriteLine("{0}と{1}の平均値は{2}", a, b, avg);
+           
+
+        }
+    }
+}
