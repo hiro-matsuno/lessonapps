b6c50deb2ea3310230a6ac006f025b8f0d8ef6e1
Hironori Matsuno <hiro.matsuno@gmail.com>
2023-02-17 06:49:13 +0000

    2023-02-17 15:48

---
+++ ExampleProblem307/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProblem307{
+    class program{
+        static void Main(string[] args){
+            int i = 0;
+            int j = 0;
+
+            Random rnd = new Random();
+            int[] n = new int[10];
+        
+            for(i = 0; i < n.Length; i++){
+                int num = rnd.Next(1, 101);
+                n[i] = num;
+                Console.Write("{0} ",num);
+            }
+            Console.WriteLine();
+            string[] names= {"奇数","偶数"};
+            for (i = 0; i <= 1; i++){
+                Console.Write(names[i]);
+                for(j = 0; j < n.Length; j++){
+                    if(n[j] % 2 == i){
+                        Console.Write("{0} ", n[j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ExampleProblem308/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProblem308{
+    class program{
+         static void Main(string[] args){
+            Random rnd = new Random();
+            int[] start = new int[10];
+            for (int i = 0; i < start.Length; i++){
+                start[i] = rnd.Next(1,101);
+                Console.Write("{0} ",start[i]);
+            }
+            Console.WriteLine();
+
+            for(int i = 0; i < start.Length; i++){
+                int max = 0;
+                int pos = 0;
+
+                for (int j = 0;j < start.Length; j++){
+                    if(max < start[i]){
+                        max = start[j];
+                        pos = j; 
+                    }
+                    Console.Write("{0} ",max);
+                    start[pos] = 0;
+                }
+                Console.WriteLine();
+            }
+         }
+    }
+}
