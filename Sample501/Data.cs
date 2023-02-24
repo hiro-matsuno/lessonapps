9131f083792a565b4121de6bd0120c7b5ad7b158
Hironori Matsuno <hiro.matsuno@gmail.com>
2023-02-24 06:41:49 +0000

    2023-02-24 15:41

---
+++ Sample501/Data.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample501{
+    class Data{
+        private static int num = 0;
+        private int id;
+
+        public Data(int id){
+            this.id = id;
+            num++;
+            Console.WriteLine("値:{0} 数:{1}",this.id, num);
+        }
+
+        public static void ShowNumber(){
+            Console.WriteLine("Dataオブジェクトの数:{0}",num);
+        }
+    }
+}
---
+++ Sample501/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample501{
+    class Program{
+        static void Main(string[] args){
+            Data[] d = new Data[2];
+            Data.ShowNumber();
+            for (int i=0; i< d.Length; i++){
+                d[i] = new Data(1*100);
+                Data.ShowNumber();
+            }
+        }
+    }
+}
