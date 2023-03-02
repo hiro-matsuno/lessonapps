9883ab6b9443ba044250f7ec9a5210b575462093
Hironori Matsuno <hiro.matsuno@gmail.com>
2023-03-02 06:40:16 +0000

    2023-03-02 15:40

---
+++ Sample605/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample605{
+    delegate void Oparation(int a, int b);
+
+    class Calc{
+        public void Sub(int a, int b){
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+        }
+    }
+
+    class Program{
+        static void Add(int a, int b){
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+        }
+
+        static void Main(string[] args){
+            Calc c = new Calc();
+            Oparation o1 = new Oparation(Add);
+            Oparation o2 = new Oparation(c.Sub);
+
+            o1(2,1);
+            o2(2,1);
+        }
+    }
+}
---
+++ Sample606/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample606{
+    class Program{
+        delegate void Action(int a);
+        static void Func1(int a){
+            Console.WriteLine("a={0}",a);
+        }
+
+        static void Func2(int a){
+            Console.WriteLine("a*2={0}",a * 2);
+        }
+
+        static void Func3(int a){
+            Console.WriteLine("a*3={0}",a * 3);
+        }
+
+        static void Main(string[] args){
+            Action a = new Action(Func1);
+            a += new Action(Func2);
+            a += new Action(Func3);
+            a(3);
+        }
+    }    
+}
